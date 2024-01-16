79d88307ba66dee64357138d18392472c79fe7fd
Khushnid <38403935+Khushnid@users.noreply.github.com>
2024-01-16 10:09:43 +0500

    Initial Commit

---
+++ RickMorty/Core/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  RickMorty
+//
+//  Created by Khushnidjon Keldiboev on 16/01/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
