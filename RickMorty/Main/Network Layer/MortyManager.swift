26b4fa85252bc56b6f94db5ac6a24359aff3fd35
Khushnid <38403935+Khushnid@users.noreply.github.com>
2025-01-06 11:01:43 +0500

    Update MortyManager.swift

--- RickMorty/Main/Network Layer/MortyManager.swift
+++ RickMorty/Main/Network Layer/MortyManager.swift
@@ -25,29 +25,21 @@ actor MortyManager {
 // MARK: Fetch all character list
 extension MortyManager {
     func fetchCharacter(link: String) async throws -> CharactersModel {
-        do {
-            guard let endpoint = URL(string: link) else { throw URLError(.badURL) }
-            let (data, response) = try await session.data(from: endpoint)
-           
-            guard validate(response: response) else { throw URLError(.badServerResponse) }
-            return try JSONDecoder().decode(CharactersModel.self, from: data)
-        } catch {
-            throw error
-        }
+        guard let endpoint = URL(string: link) else { throw URLError(.badURL) }
+        let (data, response) = try await session.data(from: endpoint)
+       
+        guard validate(response: response) else { throw URLError(.badServerResponse) }
+        return try JSONDecoder().decode(CharactersModel.self, from: data)
     }
 }
 
 // MARK: Fetch details of specific character
 extension MortyManager {
     func fetchCharacterDetails(characterID: UInt) async throws -> CharacterDetailsModel {
-        do {
-            guard let endpoint = URL(string: "\(MortyManager.charcterURL)/\(characterID)") else { throw URLError(.badURL) }
-            let (data, response) = try await session.data(from: endpoint)
-           
-            guard validate(response: response) else { throw URLError(.badServerResponse) }
-            return try JSONDecoder().decode(CharacterDetailsModel.self, from: data)
-        } catch {
-            throw error
-        }
+        guard let endpoint = URL(string: "\(MortyManager.charcterURL)/\(characterID)") else { throw URLError(.badURL) }
+        let (data, response) = try await session.data(from: endpoint)
+       
+        guard validate(response: response) else { throw URLError(.badServerResponse) }
+        return try JSONDecoder().decode(CharacterDetailsModel.self, from: data)
     }
 }
